9854688264f072bf099f07e94897840a20b1e014
eduard.hannoshyn <eduard.hannoshyn@avenga.com>
2023-03-03 15:44:40 +0200

    added globalSlice

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,27 +5,8 @@ import {setupStore} from '../store/store';
 import Todos from './Todos';
 import SafeBackgroundContainer from './SafeBackgroundContainer';
 import Notifications from './Notifications';
-import {NotificationType} from '../models/INotification';
 
 const texture = require('../assets/img/texture.jpeg');
-
-const notifications = [
-  {
-    id: '1',
-    type: NotificationType.success,
-    message: 'Success Message',
-  },
-  {
-    id: '2',
-    type: NotificationType.info,
-    message: 'Info Message',
-  },
-  {
-    id: '3',
-    type: NotificationType.failed,
-    message: 'Failed Message',
-  },
-];
 
 function App(): JSX.Element {
   const store = setupStore();
@@ -33,7 +14,7 @@ function App(): JSX.Element {
 
   return (
     <Provider store={store}>
-      <Notifications isDarkMode={isDarkMode} notifications={notifications} />
+      <Notifications isDarkMode={isDarkMode} />
       <SafeBackgroundContainer
         isDarkMode={isDarkMode}
         textureSource={texture}
